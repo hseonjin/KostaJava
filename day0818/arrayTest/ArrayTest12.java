618ace2e70c973e4a9135a8c2c5b0e0688ea36b1
Seonjin Heo <seonjin.dev@gmail.com>
2023-08-20 20:56:30 +0900

    study

--- day0818/arrayTest/ArrayTest12.java
+++ day0818/arrayTest/ArrayTest12.java
@@ -28,37 +28,38 @@ public class ArrayTest12 {
 		// 3번
 		// 평균
 		Scanner sc = new Scanner(System.in);
-		String[] subject = { "국어", "영어", "수학" };
 		int[][] score = new int[4][4];
-
-		// 점수 입력 받기
+		// 학생들의 점수 입력받기
 		for (int i = 0; i < score.length - 1; i++) {
-			System.out.println("<" + (i + 1) + "번째 학생의 점수>");
-			for (int j = 0; j < score[i].length - 1; j++) {
-				System.out.println(subject[j] + ":");
+			System.out.println(String.format("<%d번째 학생의 점수>", i + 1));
+			// 국어, 영어, 수학 순으로 입력받기 (한 번에 입력해야 하기 때문에 j에 따라 출력하는 과목명 설정)
+			for (int j = 0; j < score.length - 1; j++) {
+				if (j == 0)
+					System.out.print("국어 : ");
+				else if (j == 1)
+					System.out.print("영어 : ");
+				else
+					System.out.print("수학 : ");
 				score[i][j] = sc.nextInt();
-				score[i][score[i].length - 1] += score[i][j]; // 마지막 열에 누적
-				score[score.length - 1][j] += score[i][j]; // 마지막 행에 누적
+				score[i][score.length - 1] += score[i][j]; // 학생별 합계 저장
+				score[score.length - 1][j] += score[i][j]; 
+				// 과목별 평균 먼저 나눠서 입력할 경우 반올림 수에 따라서 값이 변동될 수 있음
 			}
 		}
 
-		// 과목별 평균
-		for (int i = 0; i < score[0].length - 1; i++) {
-			score[score.length - 1][i] /= 3;
-		}
-
-		// 학생별 총점
+		// 학생들의 점수 출력하기
 		System.out.println("\t국어\t영어\t수학\t총점");
 		for (int i = 0; i < score.length; i++) {
-			System.out.print(i + 1 + "번\t");
+			System.out.print((i + 1) + "번\t");
 			for (int j = 0; j < score.length; j++) {
-				if (score[i][j] != 0) {
-					System.out.print(score[i][j]);
-				}
-				System.out.println("\t");
+				if (i == score[i].length - 1) {
+					if (j == score.length - 1)
+						continue; // 맨 마지막 0은 출력하지 않음
+					System.out.print(score[i][j] / 3 + "\t");
+				} else
+					System.out.print(score[i][j] + "\t");
 			}
 			System.out.println();
 		}
 	}
-
 }
